ca3285a4a2ecf95fad4b607033a3233096b97252
Hamed Goma <hamednurigoma@gmail.com>
2022-03-07 03:17:43 +0200

    flutter_offline

--- lib/presentation_layer/Layout/homeNewsLayout.dart
+++ lib/presentation_layer/Layout/homeNewsLayout.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_offline/flutter_offline.dart';
 import 'package:itmc323/busniss_logic_layer/cubit/news_cubit.dart';
 import 'package:itmc323/data_layer/remote/dio.dart';
 import 'package:itmc323/presentation_layer/modules/bbcNews/bbcNews.dart';
@@ -26,6 +27,25 @@ class _HomeNewsLayoutState extends State<HomeNewsLayout>
     super.initState();
   }
 
+  Widget noEnterntWidget() {
+    return Center(
+        child: Container(
+      color: Colors.white,
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 150,
+          ),
+          const Text(
+            'Can\'t Connect Check Internet',
+            style: TextStyle(fontSize: 22, color: Colors.deepPurple),
+          ),
+          Image.asset('assets/images/noConnection.png')
+        ],
+      ),
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<NewsCubit, NewsState>(
@@ -33,45 +53,57 @@ class _HomeNewsLayoutState extends State<HomeNewsLayout>
       builder: (context, state) {
         var Cubit = NewsCubit.get(context);
         return Scaffold(
-          appBar: AppBar(
-            title: const Text(
-              "News Hero",
-            ),
-            centerTitle: true,
-            actions: [
-              Padding(
-                padding: const EdgeInsets.only(right: 10),
-                child: IconButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: ((context) => SrearchScreen())));
-                    },
-                    icon: const Icon(Icons.search_outlined)),
+            appBar: AppBar(
+              title: const Text(
+                "News Hero",
               ),
-            ],
-            bottom: TabBar(
-              isScrollable: true,
-              onTap: (value) {
-                Cubit.changeTabBar(value);
-              },
-              tabs: Cubit.tabslist,
-              controller: tabcontroller,
+              centerTitle: true,
+              actions: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 10),
+                  child: IconButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: ((context) => SrearchScreen())));
+                      },
+                      icon: const Icon(Icons.search_outlined)),
+                ),
+              ],
+              bottom: TabBar(
+                isScrollable: true,
+                onTap: (value) {
+                  Cubit.changeTabBar(value);
+                },
+                tabs: Cubit.tabslist,
+                controller: tabcontroller,
+              ),
             ),
-          ),
-
-          body: TabBarView(
-            controller: tabcontroller,
-            children: [
-              NewsLy(),
-              HeadLines(),
-              sportsScreen(),
-              BbcNews(),
-            ],
-          ),
-          //Cubit.screens[Cubit.navBarIndex],
-        );
+            body: OfflineBuilder(
+              connectivityBuilder: (
+                BuildContext context,
+                ConnectivityResult connectivity,
+                Widget child,
+              ) {
+                final bool connected = connectivity != ConnectivityResult.none;
+                if (connected) {
+                  Cubit.getAllData;
+                  return TabBarView(
+                    controller: tabcontroller,
+                    children: const [
+                      NewsLy(),
+                      HeadLines(),
+                      sportsScreen(),
+                      BbcNews(),
+                    ],
+                  );
+                } else {
+                  return noEnterntWidget();
+                }
+              },
+              child: const CircularProgressIndicator(),
+            ));
       },
     );
   }
